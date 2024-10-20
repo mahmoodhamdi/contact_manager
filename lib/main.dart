44f6c62fbc2cfcca128dd90944bdf9e499ea5b83
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-20 07:21:20 +0300

    setup hive packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:contact_manager/app/app.dart';
+import 'package:contact_manager/core/models/user_model.dart';
+import 'package:contact_manager/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserModelAdapter());
+  await Hive.openBox<UserModel>(kBox);
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(const MyApp());
